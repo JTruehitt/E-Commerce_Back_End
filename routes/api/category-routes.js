ca24ce00cea60fad2edf4703ec84834b3f5b7afd
JTrue <truehitt@gmail.com>
2023-05-11 15:55:21 -0400

    added query to update a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,35 +16,55 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   // find one category by its `id` value
-  const { id } = req.params
+  const { id } = req.params;
   // be sure to include its associated Products
   try {
     const category = await Category.findAll({
       where: {
-        id: id
+        id: id,
       },
-      include: Product
+      include: Product,
     });
-    res.status(200).json(category)
+    res.status(200).json(category);
   } catch (err) {
-    res.status(500).json({ message: `Error: ${err}`})
+    res.status(500).json({ message: `Error: ${err}` });
   }
 });
 
 router.post("/", async (req, res) => {
   // create a new category
-  const category = req.body
+  const category = req.body;
 
   try {
-    const newCategory = await Category.create(category)
-    res.status(200).json(newCategory)
+    const newCategory = await Category.create(category);
+    res.status(200).json(newCategory);
   } catch (err) {
-    res.status(500).json({ message: `Error: ${err}`})
+    res.status(500).json({ message: `Error: ${err}` });
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  const { id } = req.params;
+  const category_name = req.body;
+
+  try {
+    await Category.update(category_name, {
+      where: {
+        id: id,
+      },
+    });
+
+    const updatedCategory = await Category.findAll({
+      where: {
+        id: id,
+      },
+    });
+
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(500).json({ message: `Error: ${err}` });
+  }
 });
 
 router.delete("/:id", (req, res) => {
